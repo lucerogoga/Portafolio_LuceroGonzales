247ca8773d80256f7b872e3f0cd5f5281a68d86b
lucerogoga <lucerogoga63@hotmail.com>
2020-10-11 23:29:49 -0500

    accesibilidad con aria y js

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ window.onload = () => {
   document.querySelector(".arrow-left").addEventListener("click", clickLeft);
   document
     .querySelector(".send-button")
-    .addEventListener("click", showNotification);
+    .addEventListener("click",e=> validateForm(e));
   document.querySelectorAll(".project").forEach(element => {
     element.addEventListener("click", e => openModal(e));
   });
   document.body.addEventListener("click", e => closeModal(e));
+document.body.addEventListener("keyup",e=> listenForEsc(e))
 };
 
 /** Esta funcion se llama cuando la persona hace click en la fecha derecha del carousel para navegar a la derecha */
@@ -21,6 +22,22 @@ function clickRight() {
   }
   let newValue = currentLeft - 270; //270 toma en cuenta el tamaño de la imagen mas sus margines
   document.querySelector(".project-container").style.left = `${newValue}px`;
+switch(newValue){
+  case -270:
+  document.querySelector(".projecto1").setAttribute("tabindex", "-1");
+  document.querySelector(".project1-container").setAttribute("aria-hidden",true);
+  document.querySelector(".projecto4").setAttribute("tabindex");
+  document.querySelector(".project4-container").removeAttribute("aria-hidden");
+  break;
+  case -540:
+  document.querySelector(".projecto2").setAttribute("tabindex", "-1");
+  document.querySelector(".project2-container").setAttribute("aria-hidden",true);
+  document.querySelector(".projecto5").setAttribute("tabindex");
+  document.querySelector(".project5-container").removeAttribute("aria-hidden");
+  break;
+  default:
+  break;
+}
 }
 
 /** Esta funcion se llama cuando la persona hace click en la fecha izquierda del carousel para navegar a la izquierda */
@@ -34,21 +51,55 @@ function clickLeft() {
   }
   let newValue = currentLeft + 270;
   document.querySelector(".project-container").style.left = `${newValue}px`;
+  switch(newValue){
+    case -270:
+    document.querySelector(".projecto5").setAttribute("tabindex", "-1");
+    document.querySelector(".project5-container").setAttribute("aria-hidden",true);
+    document.querySelector(".projecto2").setAttribute("tabindex");
+    document.querySelector(".project2-container").removeAttribute("aria-hidden");
+ 
+      break;
+    case -540:
+    document.querySelector(".projecto4").setAttribute("tabindex", "-1");
+    document.querySelector(".project4-container").setAttribute("aria-hidden",true);
+    document.querySelector(".projecto1").setAttribute("tabindex");
+    document.querySelector(".project1-container").removeAttribute("aria-hidden");
+    break;
+    default:
+    break;
+  }
 }
-
+/**Validad el formulario antes de mostar la notificacion */
+function validateForm(e){
+e.preventDefault();
+const nameField=documnt.getElementById("name");
+if(nameField=""){
+document.getElementById("name-error").innerHTML="! Para enviar el formulario, se necesita un nombre";
+}else{
+  showNotification();
+}
+}
 /** Esta funcion se llama cuando la persona hace click en el boton de enviar del formulario de contacto */
 function showNotification() {
+  document.getElementById("name-error").innerHTML="";
+  document.querySelector(".form-container").reset();
   document.querySelector(".notification").style.display = "flex";
+  document.querySelector(".notification").innerHTML="El formulario se envio con exito";
+  
   setTimeout(function() {
     document.querySelector(".notification").style.display = "none";
   }, 3000);
 }
-
+/**Escucha por la clave ESC para cerrar el modal*/
+function listenForEsc(e){
+  if(e.keyCode===27){
+    closeModal(e);
+  }
+}
 /** Esta funcion se llama cuando la persona hace click en cualquier porjecto del carousel */
 function openModal(e) {
   document.querySelector(".modal-container").style.display = "flex";
-}
-
+document.getElementById("modal-header").focus();
 /** Esta funcion se llama para cerrar el modal */
 function closeModal(e) {
   // si el click occurio dentro del las imagenes del carousel o dentro del modal, no se cierra el modal
